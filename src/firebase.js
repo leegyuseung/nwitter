f5b67f0d9a3e8ae26e2814e99ffc7a28c298d935
leegyuseung <lgs4002@naver.com>
2022-08-03 21:20:22 +0900

    firebase 설정

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCY2jxh7je7wviieM6v5UU_Rhw6az69hc0",
+  authDomain: "nwitter-c1b53.firebaseapp.com",
+  projectId: "nwitter-c1b53",
+  storageBucket: "nwitter-c1b53.appspot.com",
+  messagingSenderId: "140715902546",
+  appId: "1:140715902546:web:6976084a5d3838b7a8418b",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
