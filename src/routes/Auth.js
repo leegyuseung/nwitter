8ae81f8215fba103b6666db2071023f412fe929e
leegyuseung <lgs4002@naver.com>
2022-08-04 22:51:45 +0900

    loginform 만들기

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  //Email
+  const [email, setEmail] = useState("");
+  //Password
+  const [password, setPassword] = useState("");
+
+  // Onchange 함께쓰기
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  // form 제출 Onsubmit
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
 
 export default Auth;
