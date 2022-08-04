7bcee9b0718185b997d164eb18351d401e6315d8
leegyuseung <lgs4002@naver.com>
2022-08-05 00:52:09 +0900

    로그아웃과 useNavigate 이용하기

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <BrowserRouter>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
           <>
             <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
           </>
         ) : (
           <>
