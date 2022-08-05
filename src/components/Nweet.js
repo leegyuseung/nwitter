39738f4b6de0004646b8d88df19c261bb90c5c68
leegyuseung <lgs4002@naver.com>
2022-08-05 23:56:58 +0900

    nweet 삭제하기 수정하기

---
+++ src/components/Nweet.js
@@ -0,0 +1,60 @@
+import { dbService } from "fbase";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import React, { useState } from "react";
+
+const Nweet = ({ nweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newNweet, setNewNweet] = useState(nweetObj.nweet);
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure you want to delete this nweet?");
+    if (ok) {
+      //delete neweet
+      await deleteDoc(doc(dbService, "nweets", `/${nweetObj.id}`));
+    }
+  };
+
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await updateDoc(doc(dbService, "nweets", `/${nweetObj.id}`), {
+      nweet: newNweet,
+    });
+    setEditing(false);
+  };
+  const OnChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewNweet(value);
+  };
+  return (
+    <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Edit your nweet"
+              value={newNweet}
+              onChange={OnChange}
+              required
+            />
+            <input type="submit" value="Update Nweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{nweetObj.nweet}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Nweet</button>
+              <button onClick={toggleEditing}>Edit Nweet</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+export default Nweet;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,6 +7,7 @@ import {
   orderBy,
   onSnapshot,
 } from "firebase/firestore";
+import Nweet from "components/Nweet";
 
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
@@ -54,9 +55,11 @@ const Home = ({ userObj }) => {
       </form>
       <div>
         {nweets.map((nweet) => (
-          <div key={nweet.id}>
-            <h4>{nweet.nweet}</h4>
-          </div>
+          <Nweet
+            key={nweet.id}
+            nweetObj={nweet}
+            isOwner={nweet.creatorId === userObj.uid}
+          />
         ))}
       </div>
     </div>
