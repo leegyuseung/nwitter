cf6e9fa068a6b3fcb26fe939501996340ddc433c
leegyuseung <lgs4002@naver.com>
2022-08-06 22:30:33 +0900

    URL을 이용한 사진삭제

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,5 +1,6 @@
-import { dbService } from "fbase";
+import { dbService, storageService } from "fbase";
 import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
 import React, { useState } from "react";
 
 const Nweet = ({ nweetObj, isOwner }) => {
@@ -10,6 +11,7 @@ const Nweet = ({ nweetObj, isOwner }) => {
     if (ok) {
       //delete neweet
       await deleteDoc(doc(dbService, "nweets", `/${nweetObj.id}`));
+      await deleteObject(ref(storageService, nweetObj.attachmentURL));
     }
   };
 
